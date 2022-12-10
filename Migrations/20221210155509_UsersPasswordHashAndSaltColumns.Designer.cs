b9b0e3f4e2ba6f1c1954a03d168d60976aeea55b
Salih <salihelobeid@gmail.com>
2022-12-10 10:57:28 -0500

    adds password encryption columns

--- Migrations/20221210155509_UsersPasswordHashAndSaltColumns.Designer.cs
+++ Migrations/20221210155509_UsersPasswordHashAndSaltColumns.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApp.Data;
 
 namespace WebApp.Migrations
 {
     [DbContext(typeof(DbContext_dpal))]
-    partial class DbContext_dpalModelSnapshot : ModelSnapshot
+    [Migration("20221210155509_UsersPasswordHashAndSaltColumns")]
+    partial class UsersPasswordHashAndSaltColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20221210155509_UsersPasswordHashAndSaltColumns.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class UsersPasswordHashAndSaltColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "RegisterUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "RegisterUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "RegisterUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "RegisterUsers");
+        }
+    }
+}
